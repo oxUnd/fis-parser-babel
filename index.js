23fc9d236bc2771ab15f1a4c8f10a3bee7931da3
xiangshouding <fansekey@gmail.com>
2015-06-19 07:36:37 +0800

    update

--- index.js
+++ index.js
@@ -8,6 +8,6 @@ module.exports = function (content, file, opts) {
 	file.isES6 = (file.isES6 == undefined) ? true : file.isES6;
 	if (!file.isES6) return content;
 	var result = babel.transform(content, opts);
-	file.transform = result;
+	//file.transform = result;
 	return result.code;
 };

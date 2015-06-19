f7d9e99048e0690e99d58c2647ef4a7e966fc87c
xiangshouding <fansekey@gmail.com>
2015-06-19 13:04:11 +0800

    set specail moduleId

--- index.js
+++ index.js
@@ -5,8 +5,9 @@
 var babel = require('babel-core');
 
 module.exports = function (content, file, opts) {
-	file.extras.isES6 = (file.extras.isES6 == undefined) ? true : file.extras.isES6;
-	if (!file.extras.isES6) return content;
+	file.isES6 = (file.isES6 == undefined) ? true : file.isES6;
+	if (!file.isES6) return content;
+  opts.moduleId = file.getId();
 	var result = babel.transform(content, opts);
 	return result.code;
 };

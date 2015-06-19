65b79933799aca058ac86b494b25ca226fbae126
xiangshouding <fansekey@gmail.com>
2015-06-19 12:37:07 +0800

    update

--- index.js
+++ index.js
@@ -5,9 +5,8 @@
 var babel = require('babel-core');
 
 module.exports = function (content, file, opts) {
-	file.isES6 = (file.isES6 == undefined) ? true : file.isES6;
-	if (!file.isES6) return content;
+	file.extras.isES6 = (file.extras.isES6 == undefined) ? true : file.extras.isES6;
+	if (!file.extras.isES6) return content;
 	var result = babel.transform(content, opts);
-	//file.transform = result;
 	return result.code;
 };
